18fee831d2b655b4ce7a9df6973f39d24c30ad0c
RahulDRaj <161126@ust.com>
2020-12-20 20:50:26 +0530

    React API

--- ecart_18-12-2020/src/main/java/com/ust/cart/repo/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ust.cart.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.ust.cart.entities.Product;
-
-public interface CartRepository extends CrudRepository<Product, Integer> {
-
-}
